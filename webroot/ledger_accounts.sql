8b54ba1bd8402d3b32ac8b30330c82aee5aab10a
Abhilash Lohar <abhilashlohar01@gmail.com>
2016-11-22 10:35:39 +0530

    sdfd

--- webroot/ledger_accounts.sql
+++ webroot/ledger_accounts.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 16, 2016 at 08:56 AM
--- Server version: 10.1.13-MariaDB
--- PHP Version: 5.6.20
+-- Generation Time: Nov 22, 2016 at 06:05 AM
+-- Server version: 10.1.16-MariaDB
+-- PHP Version: 7.0.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -29,8 +29,20 @@ SET time_zone = "+00:00";
 CREATE TABLE `ledger_accounts` (
   `id` int(12) NOT NULL,
   `account_second_subgroup_id` int(12) NOT NULL,
-  `name` varchar(255) NOT NULL
+  `name` varchar(255) NOT NULL,
+  `source_model` varchar(255) NOT NULL,
+  `source_id` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `ledger_accounts`
+--
+
+INSERT INTO `ledger_accounts` (`id`, `account_second_subgroup_id`, `name`, `source_model`, `source_id`) VALUES
+(1, 1, 'This is the body of the article', 'This is the body of the article', 97),
+(2, 1, 'Sdfsd', 'Customers', 98),
+(3, 1, 'Sdfsd', 'Customers', 99),
+(4, 1, 'Sdfsd', 'Customers', 100);
 
 --
 -- Indexes for dumped tables
@@ -50,7 +62,7 @@ ALTER TABLE `ledger_accounts`
 -- AUTO_INCREMENT for table `ledger_accounts`
 --
 ALTER TABLE `ledger_accounts`
-  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
