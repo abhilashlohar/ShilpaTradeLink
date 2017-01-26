5aca6d09f1fc17455b408c7f55960adbf4e772d2
Abhilash Lohar <abhilashlohar01@gmail.com>
2017-01-26 05:31:59 -0500

    sdfdsf

--- webroot/inventory_vouchers.sql
+++ webroot/inventory_vouchers.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 24, 2016 at 10:19 AM
--- Server version: 10.1.19-MariaDB
--- PHP Version: 5.6.28
+-- Generation Time: Jan 26, 2017 at 11:31 AM
+-- Server version: 10.1.13-MariaDB
+-- PHP Version: 7.0.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -29,20 +29,20 @@ SET time_zone = "+00:00";
 CREATE TABLE `inventory_vouchers` (
   `id` int(10) NOT NULL,
   `invoice_id` int(10) NOT NULL,
-  `invoice_row_id` int(10) NOT NULL,
-  `item_id` int(10) NOT NULL,
   `iv1` varchar(10) NOT NULL,
   `iv2` varchar(10) NOT NULL,
   `iv3` varchar(10) NOT NULL,
-  `iv4` varchar(10) NOT NULL
+  `iv4` varchar(10) NOT NULL,
+  `created_by` int(10) NOT NULL,
+  `company_id` int(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `inventory_vouchers`
 --
 
-INSERT INTO `inventory_vouchers` (`id`, `invoice_id`, `invoice_row_id`, `item_id`, `iv1`, `iv2`, `iv3`, `iv4`) VALUES
-(8, 1, 4, 124, 'STL', '1', 'BE-3', '16-17');
+INSERT INTO `inventory_vouchers` (`id`, `invoice_id`, `iv1`, `iv2`, `iv3`, `iv4`, `created_by`, `company_id`) VALUES
+(1, 1, 'STL', '1', 'BE-1', '16-17', 2, 25);
 
 --
 -- Indexes for dumped tables
@@ -62,7 +62,7 @@ ALTER TABLE `inventory_vouchers`
 -- AUTO_INCREMENT for table `inventory_vouchers`
 --
 ALTER TABLE `inventory_vouchers`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
