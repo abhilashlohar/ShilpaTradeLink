241f0b199201eb3613f9b524f34ae6bf3a06d67c
gopalpatel123 <gopalkrishanp3@gmail.com>
2017-01-28 18:31:54 +0530

    vfrvr

---
+++ webroot/invoice_breakups.sql
@@ -0,0 +1,66 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 28, 2017 at 02:00 PM
+-- Server version: 10.1.19-MariaDB
+-- PHP Version: 5.6.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `shilpa_treding`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `invoice_breakups`
+--
+
+CREATE TABLE `invoice_breakups` (
+  `id` int(10) NOT NULL,
+  `invoice_id` int(10) NOT NULL,
+  `receipt_voucher_id` int(10) NOT NULL,
+  `amount` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `invoice_breakups`
+--
+
+INSERT INTO `invoice_breakups` (`id`, `invoice_id`, `receipt_voucher_id`, `amount`) VALUES
+(9, 23, 1, 12321),
+(10, 23, 2, 100),
+(11, 23, 3, 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `invoice_breakups`
+--
+ALTER TABLE `invoice_breakups`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `invoice_breakups`
+--
+ALTER TABLE `invoice_breakups`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
